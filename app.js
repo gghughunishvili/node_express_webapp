cc97bb38adb8285bbd1549930b909ca17d83d5d1
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2020-08-13 16:01:39 +0400

    added chalk, debug and morgan packages;

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
+var chalk = require('chalk');
+var debug = require('debug')('app');
+var morgan = require('morgan');
 
 var app = express();
 
+app.use(morgan('tiny'));
 app.get('/', function(req, res) {
     res.send('Hello from my library');
 });
 
 app.listen(3000, function() {
-    console.log('listening on port 3000...');
+    debug(`listening on port 3000. ${chalk.green('Check http://localhost:3000')}`);
 });
