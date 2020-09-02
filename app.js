c478164570cdf474c5a9f517929ea808d35cfef2
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2020-09-02 13:20:32 +0400

    added eslint and fixed code style for app.js;

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,10 +12,10 @@ app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.listen(3000, function() {
-    debug(`listening on port 3000. ${chalk.green('Check http://localhost:3000')}`);
+app.listen(3000, () => {
+  debug(`listening on port 3000. ${chalk.green('Check http://localhost:3000')}`);
 });
